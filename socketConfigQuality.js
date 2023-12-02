9f458e09576c0d9229983d08beb2b6d4b161d570
HafidilAsad <hafidilaksad@gmail.com>
2023-12-02 15:37:01 +0700

    change query

--- socketConfigQuality.js
+++ socketConfigQuality.js
@@ -1,5 +1,6 @@
 const socketIO = require("socket.io");
 const dbConnection = require("./dbConfig2.js");
+const axios = require("axios");
 
 const fetchDataFromDB = (socket) => {
   const query =
@@ -21,38 +22,65 @@ const fetchDataJsu = (socket) => {
   });
 };
 
+// const fetchDataQtime = (socket) => {
+//   // const today = new Date().toISOString().split("T")[0];
+//   // const query =
+//   //   "SELECT no_mesin, nrp, nama_part, " +
+//   //   'MAX(CASE WHEN q_time = "Q1" THEN judge ELSE "-" END) AS Q1, ' +
+//   //   'MAX(CASE WHEN q_time = "Q2" THEN judge ELSE "-" END) AS Q2, ' +
+//   //   'MAX(CASE WHEN q_time = "Q3" THEN judge ELSE "-" END) AS Q3, ' +
+//   //   'MAX(CASE WHEN q_time = "Q4" THEN judge ELSE "-" END) AS Q4 ' +
+//   //   "FROM db_q_time " +
+//   //   "WHERE createdAt >= ? " +
+//   //   "GROUP BY no_mesin, nrp, nama_part";
+//   // dbConnection.query(query, [today], (error, results) => {
+//   //   if (error) throw error;
+//   //   socket.emit("qualityQtime", results);
+//   // });
+// };
+
 const fetchDataQtime = (socket) => {
-  const today = new Date().toISOString().split("T")[0];
-  const query =
-    "SELECT no_mesin, nrp, nama_part, " +
-    'MAX(CASE WHEN q_time = "Q1" THEN judge ELSE "-" END) AS Q1, ' +
-    'MAX(CASE WHEN q_time = "Q2" THEN judge ELSE "-" END) AS Q2, ' +
-    'MAX(CASE WHEN q_time = "Q3" THEN judge ELSE "-" END) AS Q3, ' +
-    'MAX(CASE WHEN q_time = "Q4" THEN judge ELSE "-" END) AS Q4 ' +
-    "FROM db_q_time " +
-    "WHERE createdAt >= ? " +
-    "GROUP BY no_mesin, nrp, nama_part";
-  dbConnection.query(query, [today], (error, results) => {
-    if (error) throw error;
-    socket.emit("qualityQtime", results);
-  });
+  let url = `http://localhost:5000/QtimeSummary`;
+
+  axios
+    .get(url)
+    .then((response) => {
+      socket.emit("qualityQtime", response.data);
+    })
+    .catch((error) => {
+      console.error("Error fetching data:", error);
+    });
 };
+
 const fetchDataTrial = (socket) => {
-  const today = new Date().toISOString().split("T")[0];
-  const query =
-    "SELECT no_mesin, nrp, nama_part, " +
-    'MAX(CASE WHEN trial = "T1" THEN judge ELSE "-" END) AS T1, ' +
-    'MAX(CASE WHEN trial = "T2" THEN judge ELSE "-" END) AS T2, ' +
-    'MAX(CASE WHEN trial = "T3" THEN judge ELSE "-" END) AS T3, ' +
-    'MAX(CASE WHEN trial = "T4" THEN judge ELSE "-" END) AS T4 ' +
-    "FROM db_trial_next_process " +
-    "WHERE createdAt >= ? " +
-    "GROUP BY no_mesin, nrp, nama_part";
-  dbConnection.query(query, [today], (error, results) => {
-    if (error) throw error;
-    socket.emit("qualityTrial", results);
-  });
+  let url = "http://localhost:5000/TrialNextProcessSummary";
+
+  axios
+    .get(url)
+    .then((response) => {
+      socket.emit("qualityTrial", response.data);
+    })
+    .catch((error) => {
+      console.error("Error fetching data:", error);
+    });
 };
+
+// const fetchDataTrial = (socket) => {
+//   const today = new Date().toISOString().split("T")[0];
+//   const query =
+//     "SELECT no_mesin, nrp, nama_part, " +
+//     'MAX(CASE WHEN trial = "T1" THEN judge ELSE "-" END) AS T1, ' +
+//     'MAX(CASE WHEN trial = "T2" THEN judge ELSE "-" END) AS T2, ' +
+//     'MAX(CASE WHEN trial = "T3" THEN judge ELSE "-" END) AS T3, ' +
+//     'MAX(CASE WHEN trial = "T4" THEN judge ELSE "-" END) AS T4 ' +
+//     "FROM db_trial_next_process " +
+//     "WHERE createdAt >= ? " +
+//     "GROUP BY no_mesin, nrp, nama_part";
+//   dbConnection.query(query, [today], (error, results) => {
+//     if (error) throw error;
+//     socket.emit("qualityTrial", results);
+//   });
+// };
 
 const emitQualityData = (socket) => {
   setInterval(() => {
